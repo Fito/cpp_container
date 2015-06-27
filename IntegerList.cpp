1e014d2e1bc0da86d3be0d5a7eec2fdadb4623e2
brook <brookthomas@gmail.com>
2015-06-27 13:07:13 -0700

    methods now return nullptr when use improperly, added starter doxygen comments

--- IntegerList.cpp
+++ IntegerList.cpp
@@ -1,11 +1,18 @@
+/** \file IntegerList.cpp */
+
 #include <iostream>
 #include "IntegerList.h"
 
+/** Default constructor class. Takes no arguments */
 IntegerList::IntegerList():
   list(new int[0]),
   length(0)
   {};
 
+/**
+    Pushes a new value to the top of the list.
+    \param value int An integer value to add to the top of the list.
+*/
 void IntegerList::push(int value) {
   length++;
   int *newList = new int[length];
@@ -19,7 +26,18 @@ void IntegerList::push(int value) {
   list = newList;
 }
 
+/**
+    Removes and returns a single value from the top of the list.
+    Returns nullptr if the list is currently empty.
+
+    \returns int The top list element, or nullptr if the list is empty.
+*/
 int IntegerList::pop() {
+
+    if (length == 0) {
+        return nullptr;
+    }
+
   int returnValue = list[0];
 
   int *newList = new int[length];
@@ -32,9 +50,14 @@ int IntegerList::pop() {
 
   delete[] list;
   list = newList;
+
   return returnValue;
 }
 
+/**
+    Pushes a new value to the bottom of the list.
+    \param value int An integer value to add to the bottom of the list.
+*/
 void IntegerList::pushEnd(int value) {
   int *newList = new int[length + 1];
 
@@ -47,7 +70,18 @@ void IntegerList::pushEnd(int value) {
   list = newList;
 }
 
+/**
+    Removes and returns a single value from the bottom of the list.
+    Returns nullptr if the list is currently empty.
+
+    \returns int The bottom list element, or nullptr if the list is empty.
+*/
 int IntegerList::popEnd() {
+
+    if (length == 0) {
+        return nullptr;
+    }
+
   int returnValue = list[--length];
   int *newList = new int[length];
 
@@ -61,10 +95,26 @@ int IntegerList::popEnd() {
   return returnValue;
 }
 
+/**
+    Returns the current length of the list. May be zero.
+    \returns int The current length of the list.
+*/
 int IntegerList::getLength() {
   return length;
 }
 
+/**
+    Given an index value, returns the element at the given index value.
+    Returns nullptr if the index position is invalid.
+    \param element int A valid list index value.
+    \returns int The integer at the given index, or nullptr if index is invalid.
+*/
 int IntegerList::getElement(int element) {
-  return list[element];
+
+    if (element < length && element >= 0) {
+        return list[element];
+    } else {
+        return nullptr;
+    }
+
 }
