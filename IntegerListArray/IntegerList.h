aa3a54777bd1e4b8f62b883fa0d87c60742fdb2c
Fito von Zastrow <fito@paragil.com>
2015-07-12 12:21:25 -0700

    Folds master branch into itself under IntegerListArray directory

---
+++ IntegerListArray/IntegerList.h
@@ -0,0 +1,34 @@
+/* IntegerList.h
+ * 6/29/15
+ * Brook Thomas - Adolfo von Zastrow
+ */
+
+/**	\file IntegerList.h
+ *	\mainpage
+ *	<h1>IntegerList</h1>
+ *	IntegerList is a List container-type specifically for storing and retrieving integers.<br>
+ *	Despite it's name, IntegerList is actually a Deque, and provides double-ended push and pop functionality.<br>
+ */
+
+#ifndef __INTEGERLIST__H__
+#define __INTEGERLIST__H__
+
+class IntegerList{
+private:
+	int *list;
+	int length;
+	void resize(int newSize);
+	void replaceList(int *newList);
+	void copyArray(int* source, int* destination);
+public:
+	IntegerList();
+	void push(int value);
+	int pop();
+	void pushEnd(int value);
+	int popEnd();
+	int getLength();
+	int getElement(int element);
+	void sort();
+};
+
+#endif
